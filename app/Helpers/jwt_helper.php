c12b14d80a3ee3da8e894d83c5b8b9486ec0a28d
kumara <kumara@gmail.com>
2022-09-14 13:56:43 +0800

    ci4restapi

---
+++ app/Helpers/jwt_helper.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\ModelOtentikasi;
+use Firebase\JWT\JWT;
+
+function getJWT($otentikasiHeader)
+{
+    if (is_null($otentikasiHeader)) {
+        throw new Exception("Otentikasi JWT Gagal");
+    }
+    return explode(" ", $otentikasiHeader)[1];
+}
+function validateJWT($encodedToken)
+{
+    $key = getenv('JWT_SECRET_KEY');
+    $decodedToken = JWT::decode($encodedToken, $key, ['HS256']);
+    $modelOtentikasi = new ModelOtentikasi();
+    //xxxx.xxxx.xxxx
+    $modelOtentikasi->getEmail($decodedToken->email);
+}
+function createJWT($email)
+{
+    $key = getenv('JWT_SECRET_KEY');
+    $waktuRequest = time();
+    $waktuToken = getenv('JWT_TIME_TO_LIVE');
+    $waktuExpired = $waktuRequest + $waktuToken;
+    $payload = [
+        'email' => $email,
+        'iat' => $waktuRequest,
+        'exp' => $waktuExpired
+    ];
+    $jwt = JWT::encode($payload, $key, 'HS256');
+    return $jwt;
+}
